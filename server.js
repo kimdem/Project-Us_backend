d1140a808f5b969cc29bed13f56be43a81c7b79e
kimdem <komcss121908@gmail.com>
2025-05-09 17:09:50 +0900

    port 설정

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/api/admin", admin);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "project-us-xqqm.vercel.app",
     methods: ["GET", "POST"],
   },
 });
@@ -67,7 +67,7 @@ io.on("connection", (socket) => {
   });
 });
 
-
-server.listen(5000, () => {
-  console.log("서버 구동 성공");
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+  console.log("서버 구동 성공 :" + port);
 });
